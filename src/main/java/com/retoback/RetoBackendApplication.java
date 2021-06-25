8b014dc86f10b0edd1b9ff7054413ed375808912
rodrigo282017 <rodrigotorresrittatore@gmail.com>
2021-06-25 18:48:45 -0300

    Add swagger config

--- src/main/java/com/retoback/RetoBackendApplication.java
+++ src/main/java/com/retoback/RetoBackendApplication.java
@@ -2,8 +2,10 @@ package com.retoback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfig.class)
 public class RetoBackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/retoback/controller/ClientController.java
+++ src/main/java/com/retoback/controller/ClientController.java
@@ -6,15 +6,13 @@ import com.retoback.service.ClientService;
 import lombok.extern.slf4j.Slf4j;
 import com.retoback.model.ClientDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
 @Slf4j
 public class ClientController {
 
